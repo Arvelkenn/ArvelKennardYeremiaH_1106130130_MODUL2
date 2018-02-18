53ff9b07f4283532717b18d60f623f5af1baaf2e
septiansony <septiansony009@gmail.com>
2018-02-18 19:46:50 +0700

    initiate project

---
+++ app/src/main/java/com/example/arvel/arvelkennardyeremiah_1106130130_modul2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.arvel.arvelkennardyeremiah_1106130130_modul2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
